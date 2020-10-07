ea01d32d0f1a119d57963cbe11a2c9d4aa4c4723
Mohamed Fathi <sharksmardo@gmail.com>
2020-10-07 05:56:29 +0200

    Add initial directories for the web service
    -created folder called core which contains (initialize.php file) which contain some defines
    -created foler called includes which contains(config.php file) which contain DB connection

---
+++ ExchangeG_rest/core/initialize.php
@@ -0,0 +1,14 @@
+<?php
+
+    //define('DS') ? null :define('DS' , DIRECTORY_SEPARATOR);
+    //define('SITE_ROOT') ? null :define('SITE_ROOT' , $_SERVER['DOCUMENT_ROOT'].DS.'army_app'.DS.'army_app_rest');
+    //define('INC_PATH') ? null : define('INC_PATH' , SITE_ROOT .DS .'includes');
+    //define('COR_PATH') ? null : define('COR_PATH' , SITE_ROOT .DS .'core');
+
+
+    define('DS' , DIRECTORY_SEPARATOR);
+    define('SITE_ROOT' , $_SERVER['DOCUMENT_ROOT'].DS.'ExchangeG_web_service'.DS.'ExchangeG_rest');
+    define('INC_PATH' , SITE_ROOT .DS .'includes');
+    define('COR_PATH' , SITE_ROOT .DS .'core');
+
+?>
